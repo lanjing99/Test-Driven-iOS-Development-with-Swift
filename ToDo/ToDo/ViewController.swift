4db18d420e789f3a7421848891eaaf31a4ff573e
mww0595 <mww0595@gmail.com>
2018-06-06 07:39:08 +0800

    init ToDo project

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by lanjing on 2018/6/6.
+//  Copyright © 2018年 lanjing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
